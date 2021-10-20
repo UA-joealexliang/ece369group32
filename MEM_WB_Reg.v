ccbe3567f84767d35fe12780341eabae0caafa6c
Cameron Matsumoto <cmatsumoto@email.arizona.edu>
2021-10-19 22:08:35 -0700

    added new signals to pipeline registers

--- MEM_WB_Reg.v
+++ MEM_WB_Reg.v
@@ -20,17 +20,17 @@
 //////////////////////////////////////////////////////////////////////////////////
 
 
-module MEM_WB_Reg(MEM_RegWrite, MEM_MemtoReg, MEM_ReadData, MEM_ALUResult, MEM_RegDstData, HI, LO, func,
+module MEM_WB_Reg(MEM_RegWrite, MEM_RegWrite2, MEM_MemtoReg, MEM_ReadData, MEM_ALUResult, MEM_RegDstData, HI, LO, func, MEM_Zero,
                     Clk, Clr, Ld,
-                    WB_RegWrite, WB_MemtoReg, WB_ReadData, WB_ALUResult, WB_RegDstData, WB_HI, WB_LO, func_out);
+                    WB_RegWrite, WB_RegWrite2, WB_MemtoReg, WB_ReadData, WB_ALUResult, WB_RegDstData, WB_HI, WB_LO, func_out, WB_Zero);
         
-        input MEM_RegWrite, MEM_MemtoReg;
+        input MEM_RegWrite, MEM_RegWrite2, MEM_MemtoReg, MEM_Zero;
         input [31:0] MEM_ReadData, MEM_ALUResult, HI, LO;
         input [4:0] MEM_RegDstData;
         input [5:0] func;
         input Clk, Clr, Ld;
         
-        output reg WB_RegWrite, WB_MemtoReg;
+        output reg WB_RegWrite, WB_MemtoReg, WB_RegWrite2, WB_Zero;
         output reg [31:0]  WB_ReadData, WB_ALUResult, WB_HI, WB_LO;
         output reg [4:0] WB_RegDstData;
         output reg [5:0] func_out;
@@ -40,22 +40,26 @@ module MEM_WB_Reg(MEM_RegWrite, MEM_MemtoReg, MEM_ReadData, MEM_ALUResult, MEM_R
         if(Clr == 1) begin 
             WB_MemtoReg <= 0; 
             WB_RegWrite <= 0; 
+            WB_RegWrite2 <= 0;
             WB_ALUResult <= 0; 
             WB_ReadData <= 0;
             WB_RegDstData <= 0;
             WB_HI <= 0;
             WB_LO <= 0;
             func_out <= 0;
+            WB_Zero <= 0;
         end
         else if(Ld == 1) begin
             WB_MemtoReg <= MEM_MemtoReg; 
             WB_RegWrite <= MEM_RegWrite; 
+            WB_RegWrite2 <= MEM_RegWrite2;
             WB_ALUResult <= MEM_ALUResult; 
             WB_ReadData <= MEM_ReadData;
             WB_RegDstData <= MEM_RegDstData;
             WB_HI <= HI;
             WB_LO <= LO;
             func_out <= func;
+            WB_Zero <= MEM_Zero;
         end
                       
     end
