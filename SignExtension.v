d6a12f2d2f4e9cace77c32118bcfbe8d969d8e49
Cameron Matsumoto <cmatsumoto@email.arizona.edu>
2021-10-22 13:41:10 -0700

    added Zero Extension

--- SignExtension.v
+++ SignExtension.v
@@ -5,25 +5,32 @@
 // Module - SignExtension.v
 // Description - Sign extension module.
 ////////////////////////////////////////////////////////////////////////////////
-module SignExtension(in, out);
+module SignExtension(in, out, signOrZero);
 
     /* A 16-Bit input word */
     input [15:0] in;
+    input signOrZero;
     
     /* A 32-Bit output word */
     output reg [31:0] out;
     
-    /* Fill in the implementation here ... */
     reg [15:0] extension;
     
     always@(*) begin
-        if(in[15] == 1) begin
-            extension <= 16'b1111_1111_1111_1111;
-            out <= {extension, in};
+        if(signOrZero == 1) begin
+            if(in[15] == 1) begin
+                extension <= 16'b1111_1111_1111_1111;
+                out <= {extension, in};
+            end
+            else if(in[15] == 0) begin
+                extension <= 16'b0000_0000_0000_0000;
+                out <= {extension, in};
+            end
         end
-        else if(in[15] == 0) begin
-            extension <= 16'b0000_0000_0000_0000;
-            out <= {extension, in};
+        else begin
+            if(in[15] == 0) begin
+                extension <= 16'b0000_0000_0000_0000;
+                out <= {extension, in};
         end
     end
 
