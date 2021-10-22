84119a4f35dba04bb5ac34518ca3fe3740b4244c
Ashton <ashtonrowe3.14@hotmail.com>
2021-10-22 12:21:05 -0700

    controller modifications

--- controller.v
+++ controller.v
@@ -15,7 +15,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
     output reg [4:0] ALUControl;
 
     //SignExtend: 0 for unsigned operations, 1 for signed operations
-    //ALUSrc1: 0 for rs, 1 for imm (for rotate and shift)
+    //ALUSrc2: 0 for rs, 1 for imm (for rotate and shift)
     //HI_LO_Write: 0 = noWrite, 1 = HI_Write, 2 = LO_Write 3 = HI_LO_Write
     //Branch: 1 for branches
     //Jump: 1 for jumps
@@ -29,6 +29,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b000000: begin // r-format instructions add, addu
                 RegDst = 1'b1;
                 ALUSrc = 1'b0;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b0;
                 RegWrite = 1'b1;
                 MemRead = 1'b0;
@@ -79,20 +80,24 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
 
                     6'b000000: begin // sll
                         ALUControl = 5'b00111;
+                        ALUSrc2 = 1'b1;
                     end
 
                     6'b000100: begin // sllv
                         ALUControl = 5'b00111;
+                        ALUSrc2 = 1'b1;
                     end
 
                     6'b000010: begin
                         case(Bit21)
                             1'b0: begin // srl
                                 ALUControl = 5'b01000;
+                                ALUSrc2 = 1'b1;
                             end
 
                             1'b1: begin // rotr
                                 ALUControl = 5'b01001;
+                                ALUSrc2 = 1'b1;
                             end
 
                             default: begin
@@ -105,10 +110,12 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
                         case(Bit10_6)
                             5'b00000: begin // srlv
                                 ALUControl = 5'b01000;
+                                ALUSrc2 = 1'b1;
                             end
 
                             5'b00001: begin // rotrv
                                 ALUControl = 5'b01001;
+                                ALUSrc2 = 1'b1;
                             end
 
                             default: begin
@@ -158,6 +165,11 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
                     6'b010010: begin // mflo
                         ALUControl = 5'b10010;
                     end
+                    
+                    6'b001000: begin // jr
+                        ALUControl = 5'b11111;
+                        Jump = 1'b1;
+                    end
 
                     default: begin
                         ALUControl = 5'b11111;
@@ -168,6 +180,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b011100: begin // mul, madd, msub
                 RegDst = 1'b1;
                 ALUSrc = 1'b0;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b0;
                 RegWrite = 1'b1;
                 MemRead = 1'b0;
@@ -204,6 +217,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b100011: begin // lw
                 RegDst = 1'b0;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b1;
                 RegWrite = 1'b1;
                 MemRead = 1'b1;
@@ -217,6 +231,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b100001: begin // lh
                 RegDst = 1'b0;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b1;
                 RegWrite = 1'b1;
                 MemRead = 1'b1;
@@ -230,6 +245,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b100000: begin // lb
                 RegDst = 1'b0;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b1;
                 RegWrite = 1'b1;
                 MemRead = 1'b1;
@@ -243,6 +259,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b001111: begin // lui
                 RegDst = 1'b0;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b0;
                 RegWrite = 1'b1;
                 MemRead = 1'b0;
@@ -256,6 +273,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b101011: begin // sw
                 //RegDst = X;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 //MemtoReg = X;
                 RegWrite = 1'b0;
                 MemRead = 1'b0;
@@ -269,6 +287,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b101001: begin // sh
                 //RegDst = X;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 //MemtoReg = X;
                 RegWrite = 1'b0;
                 MemRead = 1'b0;
@@ -282,6 +301,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b101000: begin // sb
                 //RegDst = X;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 //MemtoReg = X;
                 RegWrite = 1'b0;
                 MemRead = 1'b0;
@@ -297,6 +317,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b000001: begin // bgez, bltz
                 //RegDst = X;
                 ALUSrc = 1'b0;
+                ALUSrc2 = 1'b0;
                 //MemtoReg = X;
                 RegWrite = 1'b0;
                 MemRead = 1'b0;
@@ -320,6 +341,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b000100: begin // beq
                 //RegDst = X;
                 ALUSrc = 1'b0;
+                ALUSrc2 = 1'b0;
                 //MemtoReg = X;
                 RegWrite = 1'b0;
                 MemRead = 1'b0;
@@ -333,6 +355,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b000101: begin // bne
                 //RegDst = X;
                 ALUSrc = 1'b0;
+                ALUSrc2 = 1'b0;
                 //MemtoReg = X;
                 RegWrite = 1'b0;
                 MemRead = 1'b0;
@@ -346,6 +369,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b000111: begin // bgtz
                 //RegDst = X;
                 ALUSrc = 1'b0;
+                ALUSrc2 = 1'b0;
                 //MemtoReg = X;
                 RegWrite = 1'b0;
                 MemRead = 1'b0;
@@ -359,6 +383,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b000110: begin // blez
                 //RegDst = X;
                 ALUSrc = 1'b0;
+                ALUSrc2 = 1'b0;
                 //MemtoReg = X;
                 RegWrite = 1'b0;
                 MemRead = 1'b0;
@@ -374,6 +399,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b001001: begin // addiu
                 RegDst = 1'b0;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b0;
                 RegWrite = 1'b1;
                 MemRead = 1'b0;
@@ -387,6 +413,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b001000: begin // addi
                 RegDst = 1'b0;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b0;
                 RegWrite = 1'b1;
                 MemRead = 1'b0;
@@ -400,6 +427,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b001100: begin // andi
                 RegDst = 1'b0;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b0;
                 RegWrite = 1'b1;
                 MemRead = 1'b0;
@@ -413,6 +441,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b001101: begin // ori
                 RegDst = 1'b0;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b0;
                 RegWrite = 1'b1;
                 MemRead = 1'b0;
@@ -426,6 +455,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b001110: begin // xori
                 RegDst = 1'b0;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b0;
                 RegWrite = 1'b1;
                 MemRead = 1'b0;
@@ -439,6 +469,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b001010: begin // slti
                 RegDst = 1'b0;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b0;
                 RegWrite = 1'b1;
                 MemRead = 1'b0;
@@ -452,6 +483,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b001011: begin // sltiu
                 RegDst = 1'b0;
                 ALUSrc = 1'b1;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b0;
                 RegWrite = 1'b1;
                 MemRead = 1'b0;
@@ -467,6 +499,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
             6'b011111: begin // seh, seb
                 RegDst = 1'b1;
                 ALUSrc = 1'b0;
+                ALUSrc2 = 1'b0;
                 MemtoReg = 1'b0;
                 RegWrite = 1'b1;
                 MemRead = 1'b0;
@@ -486,6 +519,30 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
                     end
                 endcase
             end
+            
+            6'b000010: begin // j
+                RegDst = 1'b0;
+                ALUSrc = 1'b0;
+                ALUSrc2 = 1'b1;
+                MemtoReg = 1'b0;
+                RegWrite = 1'b0;
+                MemRead = 1'b0;
+                MemWrite = 1'b0;
+                Branch = 1'b0;
+                Jump = 1'b1;
+            end
+            
+            6'b000011: begin // jal
+                RegDst = 1'b0;
+                ALUSrc = 1'b0;
+                ALUSrc2 = 1'b1;
+                MemtoReg = 1'b0;
+                RegWrite = 1'b0;
+                MemRead = 1'b0;
+                MemWrite = 1'b0;
+                Branch = 1'b0;
+                Jump = 1'b1;
+            end
 
             default: begin
                 RegDst = 1'b0;
@@ -495,7 +552,7 @@ module Controller(Opcode, Bit21, Bit20_16, Bit10_6, funct, RegDst, ALUSrc, ALUSr
                 MemRead = 1'b0;
                 MemWrite = 1'b0;
                 Branch = 1'b0;
-                Jump = 1'b1;
+                Jump = 1'b0;
                 //ALUOp1 = 1'b0;
                 //ALUOp0 = 1'b0;
             end
