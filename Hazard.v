87d0ded03b1648cae4fd610901b0dd3e190d66e2
cmatsumoto17 <70248765+cmatsumoto17@users.noreply.github.com>
2021-11-08 16:36:54 -0700

    added %efford and things to Hazard

--- Hazard.v
+++ Hazard.v
@@ -1,5 +1,19 @@
 `timescale 1ns / 1ps
 
+//////////////////////////////////////////////////////////////////////////////////
+// LAB GROUP 32
+//      CAMERON MATSUMOTO, ASHTON ROWE, JOE LIANG
+//      
+//      PERCENT EFFORT:
+//          CAMERON 33%     ASHTON 33%      JOE 33%
+//
+//      5 Pipeline Stages
+//
+//      Branches Resolved in the Decode Stage
+//
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
 module Hazard(ID_EX_Rd, EX_MEM_Rd, IF_ID_Rs, ID_EX_Rs, IF_ID_Rt, ID_EX_Rt, EX_MEM_Rt, ID_EX_MemRead, EX_MEM_MemRead, ID_EX_RegWrite, EX_MEM_RegWrite, ID_EX_Branch, EX_MEM_Branch, FlushSignal);
     input [4:0] ID_EX_Rd, EX_MEM_Rd, IF_ID_Rs, ID_EX_Rs, IF_ID_Rt, ID_EX_Rt, EX_MEM_Rt;
     input ID_EX_MemRead, EX_MEM_MemRead, ID_EX_RegWrite, EX_MEM_RegWrite, ID_EX_Branch, EX_MEM_Branch;
