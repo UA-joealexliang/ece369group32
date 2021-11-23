67d9a87ba5a259c63887656597cb8af89b6a8557
JOE LIANG <joealexliang@email.arizona.edu>
2021-11-22 18:30:54 -0700

    added simple testcase

--- MISC/vbsme_phase3.s
+++ MISC/vbsme_phase3.s
@@ -10,6 +10,17 @@
 ########################################################################################################################
 .data
 
+# test 0 For the 4x4 frame size and 2X2 window size
+# small size for validation and debugging purpose
+# The result should be 0, 2
+asize0:  .word    4,  4,  2, 2    #i, j, k, l
+frame0:  .word    0,  0,  1,  2, 
+         .word    0,  0,  3,  4
+         .word    0,  0,  0,  0
+         .word    0,  0,  0,  0, 
+window0: .word    1,  2, 
+         .word    3,  4,
+
 # test 7
 # The result should be
 # 3,28 Check vertical traversal
@@ -100,9 +111,9 @@ main:
          
     # Start test 1 
     ############################################################
-    la      $a0, asize1     # 1st parameter: address of asize1[0]
-    la      $a1, frame1     # 2nd parameter: address of frame1[0]
-    la      $a2, window1    # 3rd parameter: address of window1[0] 
+    la      $a0, asize0     # 1st parameter: address of asize1[0]
+    la      $a1, frame0     # 2nd parameter: address of frame1[0]
+    la      $a2, window0    # 3rd parameter: address of window1[0] 
    
     jal     vbsme           # call function
     
